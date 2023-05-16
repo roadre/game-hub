a8b77c3b0f668cb13c10f05f4d25f7be136d777d
rolf.anderegg <rolf.anderegg@bestview.ch>
2023-05-16 16:39:51 +0200

    Fetch Games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,22 +4,29 @@ import apiClient from "../services/api-client";
 import { iGame, iFetchGamesResponse } from "../services/rawg";
 
 const GameGrid = () => {
-    const [games, setGames] = useState([]);
+    const [games, setGames] = useState<iGame[]>([]);
     const [error, setError] = useState("");
 
     useEffect(() => {
         apiClient
             .get<iFetchGamesResponse>("/games")
             .then((res) => {
-                console.log(res.data.results);
+                setGames(res.data.results);
             })
             .catch((err) => setError(err.message));
     }, []);
 
     return (
         <>
-            {error && <p>{error}</p>}
-            <div>GameGrid</div>
+            {error && <p className="text-danger">{error}</p>}
+            <div>
+                <h1>List</h1>
+                <ul>
+                    {games.map((game) => (
+                        <li key={game.id}>{game.name}</li>
+                    ))}
+                </ul>
+            </div>
         </>
     );
 };
