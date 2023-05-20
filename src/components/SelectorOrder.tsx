fac8d2a2fc1bb2a8cbc0280fac5258f7e65131f9
rolf.anderegg <rolf.anderegg@bestview.ch>
2023-05-20 12:18:53 +0200

    Enum Error behoben

--- src/components/SelectorOrder.tsx
+++ src/components/SelectorOrder.tsx
@@ -24,7 +24,7 @@ const SelectorOrder = ({ selectedOrder, onSelectOrder }: Props) => {
                         key={item}
                         onClick={(e) => onSelectOrder(item as iOrderEnum)}
                     >
-                        {iOrderEnum[item]}
+                        {item}
                     </MenuItem>
                 ))}
             </MenuList>
